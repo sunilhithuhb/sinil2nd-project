da5ae4fc7d99161b418a389fb51f7130e2d6d100
sunil kiran nayak <sunilkirannayak@gmail.com>
2023-10-17 16:23:38 +0530

    sunil

---
+++ multiplication.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class multiplication {
+    public static void calculatemultiplication(int n){
+        
+    for(int i=1;i<=10;i++){
+        
+    System.out.println(n*i);}
+}
+ /**
+ * @param args
+ */
+public static void main(String[]args){
+Scanner sc= new Scanner(System.in);
+int n=sc.nextInt();
+calculatemultiplication(n);
+ }   
+}
+
